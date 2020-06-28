6dde4227fb3db74f4b2f709169c7c450041907d4
Pongsakorn <songsuroj.pongsakorn@rm.is.tohoku.ac.jp>
2020-06-28 18:22:54 +0900

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/pongsakorn/diff_drive_ws/src
+# Build directory: /home/pongsakorn/diff_drive_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("diff_drive_description")
